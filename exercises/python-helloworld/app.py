5daf2fc78665fa0961b8fdc7f3ece7812d0ef78d
mic <micmor.m@gmail.com>
2021-06-25 22:03:49 -0400

    add logger for each route

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import json
-
+import logging
 app = Flask(__name__)
 
 
@@ -11,6 +11,8 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    # log line
+    app.logger.info('Status request successful')
     return response
 
 
@@ -21,14 +23,19 @@ def metrics():
             status=200,
             mimetype='application/json'
     )
-
+    # log line
+    app.logger.info('Metrics request successful')
     return response
 
 
 @app.route("/")
 def hello():
+    # log line
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 
 if __name__ == "__main__":
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
